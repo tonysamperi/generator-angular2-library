80ba05d6bb86bfb46db36a6a0a25d2953db2319b
Tony Samperi <tonysamperibg89@gmail.com>
2018-05-31 16:00:51 +0200

    Template regex improved
    * TemplateUrl regex improved

--- generators/app/templates/tools/gulp/inline-resources.js
+++ generators/app/templates/tools/gulp/inline-resources.js
@@ -78,7 +78,7 @@ function inlineResourcesFromString(content, urlResolver) {
  * @return {string} The content with all templates inlined.
  */
 function inlineTemplate(content, urlResolver) {
-  return content.replace(/(templateUrl:\s*)('|")([^\2]+?\.html\2)/g, function (m, templateUrl) {
+  return content.replace(/(templateUrl:\s*)(['"])([^\2]+?\.html\2)/g, function (m, templateUrl) {
     const templateFile = urlResolver(templateUrl);
     const templateContent = fs.readFileSync(templateFile, 'utf-8');
     const shortenedTemplate = templateContent
