f02d38745eed99e84721d953addd078da214af29
Tony Samperi <tonysamperibg89@gmail.com>
2018-12-17 12:24:34 +0100

    DEPRECATED
    DEPRECATED

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,41 @@
 "use strict";
-var path = require("path");
+
 var gulp = require("gulp");
-var eslint = require("gulp-eslint");
-var excludeGitignore = require("gulp-exclude-gitignore");
 var mocha = require("gulp-mocha");
 var istanbul = require("gulp-istanbul");
-var nsp = require("gulp-nsp");
 var plumber = require("gulp-plumber");
-var coveralls = require("gulp-coveralls");
+var exec = require("child_process").exec;
 
-gulp.task("static", function () {
-  return gulp.src("**/*.js")
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
-
-gulp.task("nsp", function (cb) {
-  nsp({package: path.resolve("package.json")}, cb);
+gulp.task("audit", function (cb) {
+  exec("npm audit --audit-level high", function (err, stdout, stderr) {
+    console.info("Audit complete!");
+    cb(err);
+  });
 });
 
 gulp.task("pre-test", function () {
   return gulp.src("generators/**/*.js")
-    .pipe(istanbul({
-      includeUntested: true
-    }))
-    .pipe(istanbul.hookRequire());
+  .pipe(istanbul({
+    includeUntested: true
+  }))
+  .pipe(istanbul.hookRequire());
 });
 
 gulp.task("test", ["pre-test"], function (cb) {
   var mochaErr;
 
   gulp.src("test/**/*.js")
-    .pipe(plumber())
-    .pipe(mocha({reporter: "spec", timeout: 60000}))
-    .on("error", function (err) {
-      mochaErr = err;
-    })
-    .pipe(istanbul.writeReports())
-    .on("end", function () {
-      cb(mochaErr);
-    });
+  .pipe(plumber())
+  .pipe(mocha({reporter: "spec", timeout: 60000}))
+  .on("error", function (err) {
+    mochaErr = err;
+  })
+  .pipe(istanbul.writeReports())
+  .on("end", function () {
+    cb(mochaErr);
+  });
 });
 
-gulp.task("coveralls", ["test"], function () {
-  if (!process.env.CI) {
-    return;
-  }
 
-  return gulp.src(path.join(__dirname, "coverage/lcov.info"))
-    .pipe(coveralls());
-});
-
-gulp.task("prepublish", ["nsp"]);
-gulp.task("default", ["static", "test"]);
+gulp.task("prepublish", ["test", "audit"]);
+gulp.task("default", ["test", "audit"]);
